bdbc3d17c9ec4873c2d2061bdeba9089c174c2c1
yanchen01 <ychen01@bu.edu>
2021-05-21 22:31:00 -0400

    completed microservices

--- comments/index.js
+++ comments/index.js
@@ -5,12 +5,6 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
-app.use(
-	express.urlencoded({
-		extended: true
-	})
-);
-
 app.use(cors());
 
 const commentsByPostId = {};
@@ -25,7 +19,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 	const comments = commentsByPostId[req.params.id] || [];
 
-	comments.push({ id: commentId, content });
+	comments.push({ id: commentId, content, status: 'pending' });
 
 	commentsByPostId[req.params.id] = comments;
 
@@ -35,7 +29,8 @@ app.post('/posts/:id/comments', async (req, res) => {
 			data: {
 				id: commentId,
 				content,
-				postId: req.params.id
+				postId: req.params.id,
+				status: 'pending'
 			}
 		})
 		.catch((err) => {
@@ -45,8 +40,35 @@ app.post('/posts/:id/comments', async (req, res) => {
 	res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-	console.log('Received Event', req.body.type);
+app.post('/events', async (req, res) => {
+	console.log('Event Received:', req.body.type);
+
+	const { type, data } = req.body;
+
+	if (type === 'CommentModerated') {
+		const { postId, id, status, content } = data;
+		const comments = commentsByPostId[postId];
+
+		const comment = comments.find((comment) => {
+			return comment.id === id;
+		});
+		comment.status = status;
+
+		await axios
+			.post('http://localhost:4005/events', {
+				type: 'CommentUpdated',
+				data: {
+					id,
+					status,
+					postId,
+					content
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+
 	res.send({});
 });
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -10,8 +10,12 @@ app.use(
 	})
 );
 
+const events = [];
+
 app.post('/events', (req, res) => {
 	const event = req.body;
+
+	events.push(event);
 
 	axios.post('http://localhost:4000/events', event).catch((err) => {
 		console.log(err);
@@ -22,8 +26,15 @@ app.post('/events', (req, res) => {
 	axios.post('http://localhost:4002/events', event).catch((err) => {
 		console.log(err);
 	});
+	axios.post('http://localhost:4003/events', event).catch((err) => {
+		console.log(err);
+	});
 
 	res.send({ status: 'OK' });
+});
+
+app.get(events, (req, res) => {
+	res.send(events);
 });
 
 app.listen(4005, () => {
